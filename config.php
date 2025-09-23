8495583317ba64ead68c90d4ee2209ae0d14ac72
Jakub M <feropalacinka@gmail.com>
2025-09-23 11:15:28 +0200

    Add files via upload

---
+++ config.php
@@ -0,0 +1,5 @@
+<?php
+
+define("WEB_AUTHOR","Jakub Marko");
+define("COMPANY_NAME","Marko.sk");
+define("COMPANY_ADRESS_1","Krizovatka 17");
---
+++ index.php
@@ -0,0 +1,18 @@
+<?php
+include("./config.php");
+
+?>
+<!DOCTYPE html>
+<html lang="sk">
+<head>
+<title><?php echo(COMPANY_NAME); ?> - <?php echo (COMPANY_ADRESS_1);?></title>
+
+<link rel="stylesheet" href="root.css">
+
+</head>
+
+<body>
+<h1>WEB_AUTHOR</h1>
+<p1>COMPANY_NAME</p1>
+</body>
+</html>
